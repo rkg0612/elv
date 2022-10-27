bde46709071a2afe0f695dd80f4c3754da82e03d
Ralph Klein Guevarra <ralphkleinguevarra@gmail.com>
2022-10-27 14:20:31 +0800

    update footer

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -489,19 +489,8 @@
         </div>
     </div>
 </div>
-<div class="position-relative overflow-hidden row">
+<div class="position-relative overflow-hidden pb-4 row">
     <div class="col-md-12 text-center">
-        <a class="btn btn-outline-secondary btn-lg" href="javascript:void(0);">Apply Today</a>
-    </div>
-</div>
-<hr>
-<div class="position-relative overflow-hidden p-3 p-md-5 text-center bg-dark text-bg-dark">
-    <div class="col-md-12 p-lg-5 my-5">
-        <h1 class="display-4 fw-normal">Transforming Business Owners Into Top 1% Industry Experts</h1>
-        <h2 class="display-6">Powerful content marketing crafted for your audience.</h2>
-        <p class="lead fw-normal">
-            If you're a founder, brand or company interested in growing your audience through high-profile content marketing, reach out to our team to start a consultation.
-        </p>
         <a class="btn btn-outline-secondary btn-lg" href="javascript:void(0);">Apply Today</a>
     </div>
 </div>
--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,12 +1,22 @@
-<div class="container">
-    <footer class="py-3 my-4">
-        <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-            <li class="nav-item"><a href="javascript:void(0);" class="nav-link px-2 text-muted">Team</a></li>
-            <li class="nav-item"><a href="javascript:void(0);" class="nav-link px-2 text-muted">Publications</a></li>
-            <li class="nav-item"><a href="javascript:void(0);" class="nav-link px-2 text-muted">FAQ's</a></li>
-            <li class="nav-item"><a href="javascript:void(0);" class="nav-link px-2 text-muted">Insights</a></li>
-            <li class="nav-item"><a href="javascript:void(0);" class="nav-link px-2 text-muted">Apply</a></li>
-        </ul>
-        <p class="text-center text-muted">&copy; 2022 ELV Agency</p>
-    </footer>
+<div class="container-fluid">
+    <div class="position-relative overflow-hidden p-3 p-md-5 text-center bg-dark text-bg-dark row">
+        <div class="col-md-12 p-lg-5 my-5">
+            <h1 class="display-4 fw-normal">Transforming Business Owners Into Top 1% Industry Experts</h1>
+            <h2 class="display-6">Powerful content marketing crafted for your audience.</h2>
+            <p class="lead fw-normal">
+                If you're a founder, brand or company interested in growing your audience through high-profile content marketing, reach out to our team to start a consultation.
+            </p>
+            <a class="btn btn-outline-light btn-lg" href="javascript:void(0);">Apply Today</a>
+        </div>
+        <footer class="bg-dark text-bg-dark">
+            <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+                <li class="nav-item"><a href="javascript:void(0);" class="nav-link px-2 text-muted">Team</a></li>
+                <li class="nav-item"><a href="javascript:void(0);" class="nav-link px-2 text-muted">Publications</a></li>
+                <li class="nav-item"><a href="javascript:void(0);" class="nav-link px-2 text-muted">FAQ's</a></li>
+                <li class="nav-item"><a href="javascript:void(0);" class="nav-link px-2 text-muted">Insights</a></li>
+                <li class="nav-item"><a href="javascript:void(0);" class="nav-link px-2 text-muted">Apply</a></li>
+            </ul>
+            <p class="text-center text-muted">&copy; 2022 ELV Agency</p>
+        </footer>
+    </div>
 </div>
