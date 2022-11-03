37f69957791253d026afe2233566bce311c47dbf
Ralph Klein Guevarra <ralphkleinguevarra@gmail.com>
2022-11-03 09:11:41 +0800

    change footer background

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,5 +1,5 @@
 <div class="container-fluid">
-    <div class="position-relative overflow-hidden p-3 p-md-5 text-center bg-dark text-bg-dark row">
+    <div class="position-relative overflow-hidden p-3 p-md-5 text-center bg-secondary text-bg-dark row">
         <div class="col-md-12 p-lg-5 my-5">
             <h1 class="display-4 fw-normal">Transforming Business Owners Into Top 1% Industry Experts</h1>
             <h2 class="display-6">Powerful content marketing crafted for your audience.</h2>
@@ -8,16 +8,16 @@
             </p>
             <a class="btn btn-outline-light btn-lg" href="javascript:void(0);">Apply Today</a>
         </div>
-        <footer class="bg-dark text-bg-dark">
+        <footer class="bg-secondary text-bg-dark">
             <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-                <li class="nav-item"><a href="javascript:void(0);" class="nav-link px-2 text-muted">Team</a></li>
-                <li class="nav-item"><a href="javascript:void(0);" class="nav-link px-2 text-muted">Publications</a></li>
-                <li class="nav-item"><a href="javascript:void(0);" class="nav-link px-2 text-muted">FAQ's</a></li>
-                <li class="nav-item"><a href="javascript:void(0);" class="nav-link px-2 text-muted">Insights</a></li>
-                <li class="nav-item"><a href="javascript:void(0);" class="nav-link px-2 text-muted">Apply</a></li>
-                <li class="nav-item"><a href="javascript:void(0);" class="nav-link px-2 text-muted">Confidentiality</a></li>
+                <li class="nav-item"><a href="javascript:void(0);" class="nav-link px-2 text-black">Team</a></li>
+                <li class="nav-item"><a href="javascript:void(0);" class="nav-link px-2 text-black">Publications</a></li>
+                <li class="nav-item"><a href="javascript:void(0);" class="nav-link px-2 text-black">FAQ's</a></li>
+                <li class="nav-item"><a href="javascript:void(0);" class="nav-link px-2 text-black">Insights</a></li>
+                <li class="nav-item"><a href="javascript:void(0);" class="nav-link px-2 text-black">Apply</a></li>
+                <li class="nav-item"><a href="javascript:void(0);" class="nav-link px-2 text-black">Confidentiality</a></li>
             </ul>
-            <p class="text-center text-muted">&copy; 2022 ELV Agency</p>
+            <p class="text-center text-black">&copy; 2022 ELV Agency</p>
         </footer>
     </div>
 </div>
