440e85a4bd288aa706c33ac4c7438f7fb4ea430a
Ralph Klein Guevarra <ralphkleinguevarra@gmail.com>
2022-11-10 20:46:43 +0800

    componentize elv agency posts

--- resources/views/pages/insights.blade.php
+++ resources/views/pages/insights.blade.php
@@ -79,6 +79,16 @@
         text-transform: uppercase;
         font-size: 12px;
     }
+
+    .cropped {
+        height: 20rem;
+        width: 100%;
+        overflow: hidden;
+    }
+
+    .cropped img {
+        width: 100%;
+    }
 </style>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.css" integrity="sha512-UTNP5BXLIptsaj5WdKFrkFov94lDx+eBvbKyoe1YAfjeRPC+gT5kyZ10kOHCfNZqEui1sxmqvodNUx3KbuYI/A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.css" integrity="sha512-OTcub78R3msOCtY3Tc6FzeDJ8N9qvQn1Ph49ou13xgA9VsH9+LRxoFU6EqLhW4+PKRfU+/HReXmSZXHEkpYoOA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -89,97 +99,7 @@
         <h1 class="display-4 fw-normal">Internal Blogs and Published Posts</h1>
     </div>
 </div>
-<div class="position-relative overflow-hidden p-3 p-md-5 bg-light row">
-    <div class="col-12">
-        <div class="company-header-content">ELV Agency Posts by Bruce Caven</div>
-        <div id="block-article" class="owl-carousel owl-theme">
-            <div class="item">
-                <div>
-                    <div class="post-image mb-3">
-                        <a href="javascript:void(0);">
-                            <img width="370" height="278" src="https://media.istockphoto.com/photos/glad-to-work-with-you-picture-id951514270?b=1&k=20&m=951514270&s=612x612&w=0&h=yp3UNtxVwiJZV3EHQqqKaS3md7HtBkVwHvH5uhcfcPE=" alt="Image 1">
-                        </a>
-                    </div>
-                    <div class="post-content-block">
-                        <h3 class="mb-1 fw-bold">
-                            <a href="javascript:void(0);">This is an example article title for this section</a>
-                        </h3>
-                        <div class="meta-data-post d-flex align-items-center flex-wrap">
-                            <a href="javascript:void(0);">
-                                <img src="https://cdn4.iconfinder.com/data/icons/small-n-flat/24/user-512.png" alt="" height="96" width="96" class="avatar avatar-96 photo">
-                            </a>
-                            <a href="javascript:void(0);">Bruce Caven</a> · 1 Jan 2022
-                        </div>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo repellat distinctio molestias quos nemo vitae culpa aspernatur omnis molestiae. ...
-                    </div>
-                </div>
-            </div>
-            <div class="item">
-                <div>
-                    <div class="post-image mb-3">
-                        <a href="javascript:void(0);">
-                            <img width="370" height="278" src="https://media.istockphoto.com/photos/millennial-black-businesswoman-addressing-colleagues-at-a-corporate-picture-id1146472948?k=20&m=1146472948&s=612x612&w=0&h=uJMkt7Jtt7sv73o77K3WcqleE6Jlsl5fhKG_6ug1jPA=" alt="Image 1">
-                        </a>
-                    </div>
-                    <div class="post-content-block">
-                        <h3 class="mb-1 fw-bold">
-                            <a href="javascript:void(0);">This is an example article title for this section</a>
-                        </h3>
-                        <div class="meta-data-post d-flex align-items-center flex-wrap">
-                            <a href="javascript:void(0);">
-                                <img src="https://cdn4.iconfinder.com/data/icons/small-n-flat/24/user-512.png" alt="" height="96" width="96" class="avatar avatar-96 photo">
-                            </a>
-                            <a href="javascript:void(0);">Bruce Caven</a> · 1 Jan 2022
-                        </div>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo repellat distinctio molestias quos nemo vitae culpa aspernatur omnis molestiae. ...
-                    </div>
-                </div>
-            </div>
-            <div class="item">
-                <div>
-                    <div class="post-image mb-3">
-                        <a href="javascript:void(0);">
-                            <img width="370" height="278" src="https://st.depositphotos.com/1594308/2118/i/950/depositphotos_21186965-stock-photo-discussion.jpg" alt="Image 1">
-                        </a>
-                    </div>
-                    <div class="post-content-block">
-                        <h3 class="mb-1 fw-bold">
-                            <a href="javascript:void(0);">This is an example article title for this section</a>
-                        </h3>
-                        <div class="meta-data-post d-flex align-items-center flex-wrap">
-                            <a href="javascript:void(0);">
-                                <img src="https://cdn4.iconfinder.com/data/icons/small-n-flat/24/user-512.png" alt="" height="96" width="96" class="avatar avatar-96 photo">
-                            </a>
-                            <a href="javascript:void(0);">Bruce Caven</a> · 1 Jan 2022
-                        </div>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo repellat distinctio molestias quos nemo vitae culpa aspernatur omnis molestiae. ...
-                    </div>
-                </div>
-            </div>
-            <div class="item">
-                <div>
-                    <div class="post-image mb-3">
-                        <a href="javascript:void(0);">
-                            <img width="370" height="278" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTY0_O66gvtoNLLzycx4kJ2l46JBuxN9JbDe1wMQL0jLbDr8zddZt86KYNBZ0_OSm9hL1U&usqp=CAU" alt="Image 1">
-                        </a>
-                    </div>
-                    <div class="post-content-block">
-                        <h3 class="mb-1 fw-bold">
-                            <a href="javascript:void(0);">This is an example article title for this section</a>
-                        </h3>
-                        <div class="meta-data-post d-flex align-items-center flex-wrap">
-                            <a href="javascript:void(0);">
-                                <img src="https://cdn4.iconfinder.com/data/icons/small-n-flat/24/user-512.png" alt="" height="96" width="96" class="avatar avatar-96 photo">
-                            </a>
-                            <a href="javascript:void(0);">Bruce Caven</a> · 1 Jan 2022
-                        </div>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo repellat distinctio molestias quos nemo vitae culpa aspernatur omnis molestiae. ...
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+@include('includes.agency-posts')
 <div class="position-relative overflow-hidden p-3 p-md-5 row">
     <div class="col-12">
         <div class="company-header-content pb-0">Published Posts by Bruce Caven</div>
